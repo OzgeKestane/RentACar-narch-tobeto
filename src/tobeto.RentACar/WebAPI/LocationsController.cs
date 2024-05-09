1ba6292c3a6e13b0fa460e5adf073c01df880c43
Özge Kestane <ozgekestane97@gmail.com>
2024-05-09 10:27:28 +0300

    added TurkeyLocations

---
+++ src/tobeto.RentACar/WebAPI/LocationsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI;
+[Route("api/[controller]")]
+[ApiController]
+public class LocationsController : ControllerBase
+{
+    [HttpGet("GetCountries")]
+    public IActionResult GetCountries()
+    {
+        var result = TurkeyLocations.GetLocations.GetCountries();
+        return Ok(result);
+    }
+    [HttpGet("GetCities")]
+    public IActionResult GetCities()
+    {
+        var result = TurkeyLocations.GetLocations.GetCities();
+        return Ok(result);
+    }
+    [HttpGet("GetCity")]
+    public IActionResult GetCity(int cityId)
+    {
+        var result = TurkeyLocations.GetLocations.GetCity(cityId);
+        return Ok(result);
+    }
+    [HttpGet("GetRegions")]
+    public IActionResult GetRegions()
+    {
+        var result = TurkeyLocations.GetLocations.GetRegions();
+        return Ok(result);
+    }
+    [HttpGet("GetRegion")]
+    public IActionResult GetRegion(int regionId)
+    {
+        var result = TurkeyLocations.GetLocations.GetRegion(regionId);
+        return Ok(result);
+    }
+    [HttpGet("GetDistricts")]
+    public IActionResult GetDistricts(int cityId)
+    {
+        var result = TurkeyLocations.GetLocations.GetDistricts(cityId);
+        return Ok(result);
+    }
+}
